c48fac9ff8cb79c24cd8f2a76cdb0d488f0488c3
Fagner Brack <github@fagnermartins.com>
2020-07-20 16:02:40 +1000

    Create draft of consignation sales agreement

--- start-server.js
+++ start-server.js
@@ -3,7 +3,11 @@ const ConsignationSalesAgreement = require('./src/ConsignationSalesAgreement');
 
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/:company', async (req, res) => {
+  res.redirect(`/${req.params.company}/consignation`);
+});
+
+app.get('/:company/consignation', async (req, res) => {
   const page = ConsignationSalesAgreement(req.query);
   res.send(await page.to('text/html'));
 });
