1d0ad4fd420ff4b0e01bd732ed38b1ca2ddad007
Fagner Brack <github@fagnermartins.com>
2020-07-23 00:35:51 +1000

    Remove unnecessary parameters from IndividualAdvertisingForm()

--- src/influencer/app.js
+++ src/influencer/app.js
@@ -11,30 +11,14 @@ module.exports = () => {
   app.get('/:company/individual', async (req, res) => {
     let page = IndividualAdvertisingForm(req.query);
 
-    const AVAILABLE_PRODUCTS = [{
-      text: 'Daline Nails Repair & Growth',
-      productId: '1x',
-      selected: req.query.productName === 'Daline Nails Repair & Growth'
-    }, {
-      text: 'Daline Nails Repair & Growth - Buy 4 Get 5',
-      productId: '5x',
-      selected: req.query.productName === 'Daline Nails Repair & Growth - Buy 4 Get 5'
-    }];
-
     if (Object.keys(req.query).length === 0) {
-      page = IndividualAdvertisingForm({
-        querystring: req.query,
-        availableProducts: AVAILABLE_PRODUCTS
-      });
+      page = IndividualAdvertisingForm({ querystring: req.query });
       res.send(await page.to('text/html'));
       return;
     }
 
     if (req.query.edit === 'y') {
-      page = IndividualAdvertisingForm({
-        querystring: req.query,
-        availableProducts: AVAILABLE_PRODUCTS
-      });
+      page = IndividualAdvertisingForm({ querystring: req.query });
       res.send(await page.to('text/html'));
       return;
     }
