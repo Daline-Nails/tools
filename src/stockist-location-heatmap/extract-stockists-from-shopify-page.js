f1f3671fb1e1b30e89a8af2497c0490aba96c7e3
Fagner Brack <github@fagnermartins.com>
2022-12-24 16:36:11 +1100

    Extract address link so customers can go to google maps and get directions to location

--- src/stockist-location-heatmap/extract-stockists-from-shopify-page.js
+++ src/stockist-location-heatmap/extract-stockists-from-shopify-page.js
@@ -2,6 +2,7 @@
   const extractName = (anchor) => anchor.innerText.split('\n')[0];
   const extractLatLng = (anchor) => [+anchor.href.split('@')[1].split(',')[0], +anchor.href.split('@')[1].split(',')[1]];
   const extractAddress = (anchor) => anchor.innerText.split('\n').slice(1).join(', ');
+  const extractAddressLink = (anchor) => anchor.href;
 
   // eslint-disable-next-line no-undef
   const anchors = [...document.querySelectorAll('#shopify-section-16185589067438de5f a')];
@@ -14,10 +15,13 @@
     const address = extractAddress(anchor);
     if (!address) continue;
 
+    const addressLink = extractAddressLink(anchor);
+    if (!addressLink) continue;
+
     const latLng = extractLatLng(anchor);
     if (!latLng) continue;
 
-    stockistList = stockistList.concat({ name, address, latLng });
+    stockistList = stockistList.concat({ name, address, addressLink, latLng });
   }
 
   return JSON.stringify(stockistList);
