b2a62d7cb88e4ec3566e92ec7e9a6892b3a2a680
Fagner Brack <github@fagnermartins.com>
2022-12-24 18:29:33 +1100

    Create script to inject iframe on the DalineNails homepage so that I can have control of sandbox

--- src/stockist-location-heatmap/app.js
+++ src/stockist-location-heatmap/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const root = require('app-root-path');
 
-const htmlContents = fs.readFileSync(`${root}/src/stockist-location-heatmap/index.html`, { encoding: 'UTF-8' });
+const heatMapHTMLContent = fs.readFileSync(`${root}/src/stockist-location-heatmap/index.html`, { encoding: 'UTF-8' });
+const testInjectHTMLContent = fs.readFileSync(`${root}/src/stockist-location-heatmap/test-inject.html`, { encoding: 'UTF-8' });
+const injectJsContent = fs.readFileSync(`${root}/src/stockist-location-heatmap/inject.js`, { encoding: 'UTF-8' });
 
 module.exports = () => {
   const app = express();
 
   app.get('/charts/stockist-location-heatmap', (req, res) => {
-    res.send(htmlContents);
+    res.send(heatMapHTMLContent);
+  });
+
+  app.get('/charts/stockist-location-heatmap/inject.js', (req, res) => {
+    res
+      .header('Content-Type', 'application/javascript')
+      .send(injectJsContent);
+  });
+
+  app.get('/charts/stockist-location-heatmap/test-inject.html', (req, res) => {
+    res.send(testInjectHTMLContent);
   });
 
   return app;
---
+++ src/stockist-location-heatmap/inject.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+(function(isBrowserSupported) {
+  if (!isBrowserSupported) return;
+  window.DalineNails = window.DalineNails || {};
+
+  window.DalineNails.injectStockistsIframe = ({ container, src, className, iframeTag }) => {
+    const iframe = document.createElement(iframeTag);
+    iframe.src = src;
+    iframe.height = '400';
+    iframe.layout = 'fill';
+    iframe.sandbox = 'allow-scripts allow-same-origin allow-popups';
+    iframe.frameborder = '0';
+    iframe.classList.add(className);
+    document.getElementById(container).appendChild(iframe);
+  };
+}(function() {
+  function supportsArrow() {
+    try {
+      eval('()=>{}');
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+  function supportsAsync() {
+    try {
+      eval('async () => {}');
+      return true;
+    } catch (e) { return false }
+  }
+  function supportsTemplateLiterals() {
+    try { return eval("''===``") }
+    catch(e) { return false; }
+  }
+  function supportsClassList() {
+    return !!document.documentElement.classList;
+  }
+
+  return (
+    !!supportsArrow() &&
+    !!supportsAsync() &&
+    !!supportsTemplateLiterals() &&
+    !!supportsClassList()
+  );
+}));
