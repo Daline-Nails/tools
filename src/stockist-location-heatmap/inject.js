1c27e8ddcf312d4a991e4efd1c19a45ba7c64350
Fagner Brack <github@fagnermartins.com>
2022-12-30 11:26:26 +1100

    inject.js: Remove the need to add iFrameTag and allow multiple class names

--- src/stockist-location-heatmap/inject.js
+++ src/stockist-location-heatmap/inject.js
@@ -1,17 +1,23 @@
 /* eslint-disable no-undef */
 (function(isBrowserSupported) {
-  if (!isBrowserSupported) return;
+  if (!isBrowserSupported) {
+    // eslint-disable-next-line no-console
+    console.error('This browser is not supported');
+    return;
+  }
   window.DalineNails = window.DalineNails || {};
 
-  window.DalineNails.injectStockistsIframe = ({ container, src, className, iframeTag }) => {
-    const iframe = document.createElement(iframeTag);
+  window.DalineNails.injectStockistsIframe = ({ container, src, classNames }) => {
+    const iframe = document.createElement('iframe');
     iframe.src = src;
     iframe.height = '400';
     iframe.layout = 'fill';
     iframe.sandbox = 'allow-modals allow-scripts allow-same-origin allow-popups allow-top-navigation';
     iframe.allow = 'geolocation *';
     iframe.frameborder = '0';
-    iframe.classList.add(className);
+    for (const className of classNames.split(' ')) {
+      iframe.classList.add(className);
+    }
     [...document.querySelectorAll(container)].forEach(containerElement => {
       containerElement.innerHTML = '';
       containerElement.appendChild(iframe);
