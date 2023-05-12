833984bf04e3b3a35e8585fb78b901f91d37f766
Fagner Brack <github@fagnermartins.com>
2023-05-12 19:30:29 +1000

    Fallback to Australia-wide stockist map whenever the accuracy is greater or equal to 100km

--- web/src/stockist-location-heatmap/resolve-initial-position.js
+++ web/src/stockist-location-heatmap/resolve-initial-position.js
@@ -1,5 +1,10 @@
-module.exports = ({ city, latitude, longitude }) => {
+const AUSTRALIA_WIDE = { lat: -29.830709201476697, lng: 134.6193490235566, zoom: 3 };
+
+module.exports = ({ city, latitude, longitude, accuracy }) => {
+  if (accuracy >= 100) {
+    return AUSTRALIA_WIDE;
+  }
   return city === 'Sydney'
     ? { lat: latitude, lng: longitude, zoom: 9 }
-    : { lat: -29.830709201476697, lng: 134.6193490235566, zoom: 3 };
+    : AUSTRALIA_WIDE;
 };
--- web/src/stockist-location-heatmap/resolve-initial-position.spec.js
+++ web/src/stockist-location-heatmap/resolve-initial-position.spec.js
@@ -7,11 +7,21 @@ describe('Initial Map Center', () => {
     const initialMapCenter = resolveInitialMapCenter({});
     expect(initialMapCenter).to.eql({ lat: -29.830709201476697, lng: 134.6193490235566, zoom: 3 });
   });
-  it('returns higher zoom if city is Sydney', () => {
+  it('returns Australia-wide if accuracy is more than 100km', () => {
     const initialMapCenter = resolveInitialMapCenter({
       latitude: -33.8478052,
       longitude: 150.6023138,
-      city: 'Sydney'
+      city: 'Sydney',
+      accuracy: 101
+    });
+    expect(initialMapCenter).to.eql({ lat: -29.830709201476697, lng: 134.6193490235566, zoom: 3 });
+  });
+  it('returns higher zoom if city is Sydney and accuracy is less than 100km', () => {
+    const initialMapCenter = resolveInitialMapCenter({
+      latitude: -33.8478052,
+      longitude: 150.6023138,
+      city: 'Sydney',
+      accuracy: 99
     });
     expect(initialMapCenter).to.eql({ lat: -33.8478052, lng: 150.6023138, zoom: 9 });
   });
