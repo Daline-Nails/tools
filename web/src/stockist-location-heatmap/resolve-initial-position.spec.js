6b5358d83fc5866ccb520dd03abfbc7a6107ac56
Fagner Brack <github@fagnermartins.com>
2023-05-15 22:10:27 +1000

    Add some sample data from different cities far from where we have stockists including their accuracies

--- web/src/stockist-location-heatmap/resolve-initial-position.spec.js
+++ web/src/stockist-location-heatmap/resolve-initial-position.spec.js
@@ -2,6 +2,19 @@ const expect = require('chai').expect;
 
 const resolveInitialMapCenter = require('./resolve-initial-position');
 
+/*
+ * Some sample data from logs for future reference:
+ *
+ * GEODATA: City "Townsville" found for IP 180.216.125.100. Geo lat: -19.3029, lon: 146.638, accuracy: 10km
+ * GEODATA: City "Townsville" found for IP 101.162.79.238. Geo lat: -19.3029, lon: 146.638, accuracy: 200km
+ * GEODATA: City "Londonderry" found for IP 58.164.79.23. Geo lat: -33.6516, lon: 150.749, accuracy: 50
+ * GEODATA: City "Nowra" found for IP 120.159.51.249. Geo lat: -34.8794, lon: 150.6039, accuracy: 100km
+ * GEODATA: City "Coffs Harbour" found for IP 49.189.45.225. Geo lat: -30.294, lon: 153.1192, accuracy: 100km
+ * GEODATA: City "Melbourne" found for IP 106.71.0.76. Geo lat: -37.8411, lon: 144.9799, accuracy: 200km
+ * GEODATA: City "Melbourne" found for IP 59.102.39.47. Geo lat: -37.8159, lon: 144.9669, accuracy: 5km
+ * GEODATA: City "Caulfield South" found for IP 115.69.41.87. Geo lat: -37.8939, lon: 145.0251, accuracy: 1000km
+ */
+
 describe('Initial Map Center', () => {
   it('returns Australia-wide map center by default when arguments are empty', () => {
     const initialMapCenter = resolveInitialMapCenter({});
