13d9180af04268e981585874461783385fd091d4
Fagner Brack <github@fagnermartins.com>
2023-05-21 15:30:21 +1000

    Do not error out if maxmind continent code property is not there

--- web/src/stockist-location-heatmap/create-ip-location.js
+++ web/src/stockist-location-heatmap/create-ip-location.js
@@ -4,12 +4,13 @@ module.exports = ipResponse => {
   const latitude = maxMindResponse.location && maxMindResponse.location.latitude ? maxMindResponse.location.latitude : 0;
   const longitude = maxMindResponse.location && maxMindResponse.location.longitude ? maxMindResponse.location.longitude : 0;
   const accuracy = maxMindResponse.location && maxMindResponse.location.accuracyRadius ? maxMindResponse.location.accuracyRadius : 9999999;
+  const continentCode = maxMindResponse.continent && maxMindResponse.continent.code ? maxMindResponse.continent.code : '';
 
   // Only log cities that I haven't yet seen in the logs
   const isCityBlacklisted = ['', 'Montenegro', 'Sydney', 'Brisbane', 'Gold Coast', 'Perth'].includes(city);
 
   // Ignore logging continents in which we have no stockists
-  const isContinentBlacklisted = ['EU', 'NA', 'AS'].includes(maxMindResponse.continent.code);
+  const isContinentBlacklisted = ['EU', 'NA', 'AS'].includes(continentCode);
 
   if (!isCityBlacklisted && !isContinentBlacklisted) {
     process.stdout.write(`GEODATA MAXMIND RESPONSE: ${JSON.stringify(ipResponse.maxMindResponse, null, 4)}\n`);
