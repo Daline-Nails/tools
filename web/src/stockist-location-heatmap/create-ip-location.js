86a328426c793c39c798f6472b4dc347e9e76310
Fagner Brack <github@fagnermartins.com>
2023-06-01 15:51:30 +1000

    Generate a file called access.txt showing every IP/latitude/longitude/accuracy of the user location so we can identify good places to stock the product later

--- web/src/stockist-location-heatmap/create-ip-location.js
+++ web/src/stockist-location-heatmap/create-ip-location.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+
+const readFileSyncIfExists = (filePath) => {
+  try {
+    return fs.readFileSync(filePath, { encoding: 'UTF-8' });
+  } catch(e) {
+    if (e.code !== 'ENOENT') throw e;
+    return '';
+  }
+};
+
 module.exports = ipResponse => {
   const maxMindResponse = ipResponse.maxMindResponse;
   const city = maxMindResponse.city ? maxMindResponse.city.names.en : '';
@@ -7,13 +18,20 @@ module.exports = ipResponse => {
   const continentCode = maxMindResponse.continent && maxMindResponse.continent.code ? maxMindResponse.continent.code : '';
 
   // Only log cities that I haven't yet seen in the logs
-  const isCityBlacklisted = ['', 'Montenegro', 'Sydney', 'Brisbane', 'Gold Coast', 'Perth'].includes(city);
+  const isCityBlacklisted = ['', 'Montenegro'].includes(city);
 
   // Ignore logging continents in which we have no stockists
   const isContinentBlacklisted = ['EU', 'NA', 'AS'].includes(continentCode);
 
   if (!isCityBlacklisted && !isContinentBlacklisted) {
-    process.stdout.write(`GEODATA MAXMIND RESPONSE: ${JSON.stringify(ipResponse.maxMindResponse, null, 4)}\n`);
+    // Store all access logs so we can use in a heatmap to identify where people are coming from
+    // to see the map and where we have to foxus on getting stockists
+    const filePath = './access.txt';
+    const newData = JSON.stringify({ city: city, ipAddress: ipResponse.ipAddress, latitude: latitude, longitude: longitude, accuracy: accuracy });
+    const existingData = readFileSyncIfExists(filePath);
+    fs.writeFileSync(filePath, existingData + '\n' + newData, { encoding: 'UTF-8' });
+
+    // process.stdout.write(`GEODATA MAXMIND RESPONSE: ${JSON.stringify(ipResponse.maxMindResponse, null, 4)}\n`);
     process.stdout.write(`GEODATA: City "${city}" found for IP ${ipResponse.ipAddress}. Geo lat: ${latitude}, lon: ${longitude}, accuracy: ${accuracy}km\n`);
   }
 
