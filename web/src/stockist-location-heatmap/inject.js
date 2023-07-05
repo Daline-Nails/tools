802bb3f90e492654699446f17a86cdef6e6015fb
Fagner Brack <github@fagnermartins.com>
2023-07-05 18:02:44 +1000

    Add API to allow for executing code on the page after stockists map is fully loaded

--- web/src/stockist-location-heatmap/inject.js
+++ web/src/stockist-location-heatmap/inject.js
@@ -1,10 +1,24 @@
 /* eslint-disable no-undef */
 (function(isBrowserSupported) {
+
+  // Declare the stockists API on top so we can return it with noop behaviour
+  // even if the browser is not supported. Otherwise the client code will
+  // try to call methods on undefined.
+  var StockistsAPI = function(iframe) {
+    return {
+      on: function(eventName, callback) {
+        if (!iframe) return;
+        if (eventName === 'load') { iframe.addEventListener('load', callback); }
+      }
+    }
+  };
+
   if (!isBrowserSupported) {
     // eslint-disable-next-line no-console
     console.error('This browser is not supported');
-    return;
+    return StockistsAPI();
   }
+
   window.DalineNails = window.DalineNails || {};
 
   window.DalineNails.injectStockistsIframe = ({ container, src, classNames }) => {
@@ -22,6 +36,7 @@
       containerElement.innerHTML = '';
       containerElement.appendChild(iframe);
     });
+    return StockistsAPI(iframe);
   };
 }(function() {
   function supportsArrow() {
